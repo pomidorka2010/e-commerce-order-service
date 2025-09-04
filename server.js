e83c1317c8f3616fac38af2ad98ae1b198c11479
pomidorka2010 <lamc560ki@hotmail.com>
2025-09-04 19:44:02 +0200

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('Welcome to the E-Commerce Order Service');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
